ce7442b05a8588fa9bea87a15787b5c2f81d4e3e
Igor Orlov <i.orlov@laconicastudio.com>
2023-02-12 14:06:48 +0100

    Web Layout privat-vuz.ua

---
+++ archive/privat-vuz.ua/js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    sliderdots('#slid ');
+});
+
+
+function sliderdots( name ){
+    var k = $(name + '> li').length, n = 0, inter = 30000;
+    goo();
+    for(var i=1; i<k; i++)  {
+        $(name + 'nav').append('<a></a>');
+    }
+    $(name + 'nav > a').click(function () {
+        clearInterval(id);
+        goo();
+        $(name + 'nav > a').removeClass( "active" );
+        $(this).addClass( "active" );
+        n = $(this).index();
+        $(name + '> li').removeClass( "active" ).eq(n).addClass( "active" );
+        $(this).parent().attr("name", n);
+        return n;
+    });
+    function goo() {
+        id = setInterval(function() {
+            n++; if(n >= k) {n = 0}
+            $(name + 'nav > a').eq(n).click();
+        }, inter);
+    }
+    $(name + 'ins').click(function () {
+        n=n-1; if(n < 0) {n = k-1} $(name + 'nav > a').eq(n).click();
+        clearInterval(id);
+        goo();
+    });
+    $(name + 'del').click(function () {
+        n=n+1;  if(n > k-1) {n = 0} $(name + 'nav > a').eq(n).click();
+        clearInterval(id);
+        goo();
+    });
+}
+
+$('.faq h3').on('click', function() {
+    if ( $(this).parent('li').hasClass('active') ) {
+        //$('.faq li').removeClass('active');
+        $(this).parent('li').removeClass('active');
+    } else if ( $(this).parent('li').not('active')) {
+        $('.faq li').removeClass('active');
+        $(this).parent('li').addClass('active');
+    }
+
+});
+
+
